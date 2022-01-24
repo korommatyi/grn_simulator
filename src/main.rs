4919e9d0014509e33d9d8969d59c2db9eda83b1f
Matyas Korom <korommatyi@users.noreply.github.com>
2022-01-24 09:27:59 +0000

    Put everything together (#9)
    * [WIP] Try to put everything together
    
    * Turn simulator to function, mutable borrow random
    
    * Turn stuff pub
    
    * Don't hold reference to system in output
    
    * System must be mutable

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use rand::{Rng, SeedableRng};
+use std::fs::File;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 mod gillespie_simulator;
 mod io;
+mod output_formatter;
 mod system;
 
 #[derive(Debug, StructOpt)]
@@ -41,7 +44,7 @@ struct Opt {
         short,
         long,
         parse(from_os_str),
-        help = "Where the write the output. The output is a csv file with the format described on \
+        help = "Where to write the output. The output is a csv file with the format described on \
                 https://github.com/korommatyi/grn_simulator/wiki."
     )]
     output: PathBuf,
@@ -50,7 +53,20 @@ struct Opt {
 fn main() {
     let opt = Opt::from_args();
 
-    let system = io::load_system(&opt.reactions, &opt.initial_state);
+    let mut system = io::load_system(&opt.reactions, &opt.initial_state);
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(opt.seed);
+    let mut output = output_formatter::CSVFormatter {
+        output: File::create(&opt.output).expect("Cannot create output file."),
+    };
+
+    output.start(&system);
+
+    for _ in 1..100 {
+        gillespie_simulator::gillespie_step(&mut system, &mut || rng.gen::<f64>());
+        output.write_current_state(&system);
+    }
+
+    output.finish(&system);
 
     println!("System:");
     println!("{:?}", system);
